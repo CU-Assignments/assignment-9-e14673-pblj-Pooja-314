3d3126733e8fe52dacf3c1b460391e6de78ca5c8
Pooja-314 <108114462+Pooja-314@users.noreply.github.com>
2025-04-18 10:56:06 +0530

    HibernateUtil.java

---
+++ HibernateUtil.java
@@ -0,0 +1,24 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory factory;
+
+    static {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return factory;
+    }
+
+    public static void closeSessionFactory() {
+        factory.close();
+    }
+}
